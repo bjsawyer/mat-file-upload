270a9cc02f704ec87de31219b7b6cceb87ad86b2
Brendan Sawyer <brendan.sawyer@excella.com>
2019-03-19 23:18:19 -0400

    Added Angular Material to demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+} from '@angular/material'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatFileUploadModule } from 'projects/mat-file-upload/src/public-api'
 
 import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatFileUploadModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFileUploadModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
