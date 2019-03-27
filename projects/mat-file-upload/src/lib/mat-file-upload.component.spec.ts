5835d6486f44d7a3f742d918d698284747698c01
Brendan Sawyer <brendan.sawyer@excella.com>
2019-03-26 21:48:53 -0400

    Adding some unit tests

--- projects/mat-file-upload/src/lib/mat-file-upload.component.spec.ts
+++ projects/mat-file-upload/src/lib/mat-file-upload.component.spec.ts
@@ -1,25 +1,59 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { MatIconModule } from '@angular/material'
 
-import { MatFileUploadComponent } from './mat-file-upload.component';
+import { MatFileUploadComponent } from './mat-file-upload.component'
 
 describe('MatFileUploadComponent', () => {
-  let component: MatFileUploadComponent;
-  let fixture: ComponentFixture<MatFileUploadComponent>;
+  let component: MatFileUploadComponent
+  let fixture: ComponentFixture<MatFileUploadComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatFileUploadComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [MatFileUploadComponent],
+      imports: [MatIconModule],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MatFileUploadComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(MatFileUploadComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  describe('filesChanged', () => {
+    it('should set selectedFileText to empty string when no files are passed in', () => {
+      component.filesChanged(null)
+      expect(component.selectedFileText).toBe('')
+    })
+
+    it('should set selectedFileText to the name of the file passed in', () => {
+      const mockFiles = mockFileList()
+      component.filesChanged(mockFiles)
+      expect(component.selectedFileText).toBe('filename')
+    })
+
+    it('should set selectedFiles to the files passed in', () => {
+      const mockFiles = mockFileList()
+      component.filesChanged(mockFiles)
+      expect(component.selectedFiles).toBe(mockFiles)
+    })
+  })
+})
+
+export function mockFileList(): FileList {
+  const blob = new Blob([''], { type: 'text/html' })
+  // tslint:disable-next-line: no-string-literal
+  blob['lastModifiedDate'] = ''
+  // tslint:disable-next-line: no-string-literal
+  blob['name'] = 'filename'
+  const file = blob as File
+  return {
+    0: file,
+    length: 1,
+    item: (index: number) => file,
+  }
+}
