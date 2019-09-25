0b37e3ca8b7309f68243360f4a4979e837d36757
Brendan Sawyer <brendan.sawyer@excella.com>
2019-09-24 21:46:52 -0400

    Switch to use CommonModule

--- projects/mat-file-upload/src/lib/mat-file-upload.module.ts
+++ projects/mat-file-upload/src/lib/mat-file-upload.module.ts
@@ -1,17 +1,17 @@
+import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 
 import { MatFileUploadComponent } from './mat-file-upload.component'
 
 @NgModule({
   declarations: [MatFileUploadComponent],
   imports: [
-    BrowserAnimationsModule,
+    CommonModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
