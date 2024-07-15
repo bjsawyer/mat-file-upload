64b3c357e610e143959b0fcdc53c2b0b16c2fde1
bjsawyer <bjsawyer@vt.edu>
2024-07-15 12:03:02 -0400

    chore: `npx ng update @angular/material@15`

--- projects/mat-file-upload/src/lib/mat-file-upload.module.ts
+++ projects/mat-file-upload/src/lib/mat-file-upload.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { MatButtonModule } from '@angular/material/button'
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button'
 import { MatIconModule } from '@angular/material/icon'
-import { MatInputModule } from '@angular/material/input'
-import { MatSelectModule } from '@angular/material/select'
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input'
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select'
 
 import { MatFileUploadComponent } from './mat-file-upload.component'
 
